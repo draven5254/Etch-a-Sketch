29c3cb3ee76d850438d2d20d5d7cbfd9fae24162
Draven <valdevisor@gmail.com>
2023-09-01 21:43:46 +0800

    initial commit

--- app.js
+++ app.js
@@ -6,14 +6,21 @@ const gridContainer = document.querySelector(".grid-container");
 const resetButton = document.getElementById("resetButton");
 const deleteButton = document.getElementById("deleteButton");
 const btnPopUp = document.querySelector(".btn");
+const colorPick = document.getElementById("colorPicker");
 
 const gridItems = [];
 
 //  new grid generate
 function createGrid(size) {
   gridContainer.innerHTML = ""; // Clear existing grid
-  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  // Calculate the column and row sizes to fit the container
+  const columnSize = `calc(100% / ${size})`;
+  const rowSize = `calc(100% / ${size})`;
+
+  // Set the column and row sizes using grid-template-columns and grid-template-rows
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, ${columnSize})`;
+  gridContainer.style.gridTemplateRows = `repeat(${size}, ${rowSize})`;
 
   for (let i = 0; i < size * size; i++) {
     const gridItem = document.createElement("div");
@@ -51,7 +58,14 @@ for (let i = 0; i < 16; i++) {
 }
 
 // change color when your mouse passes over them, leaving a (pixelated) trail
+colorPick.addEventListener("input", (event) => {
+  const selectedColor = event.target.value;
+  // Store the selected color for later use
+  currentColor = selectedColor;
+});
+
 let isDrawing = false;
+let currentColor = "black"; // Default color
 
 // Detect mouse movement and update grid item colors
 gridContainer.addEventListener("mousedown", () => {
@@ -66,7 +80,7 @@ gridContainer.addEventListener("mousemove", (event) => {
   if (isDrawing) {
     const target = event.target;
     if (target.classList.contains("grid-item")) {
-      target.style.backgroundColor = "black";
+      target.style.backgroundColor = currentColor;
     }
   }
 });
